c1f830cedc96240631db37a26770454a6e547f1c
techmefr <compigni.gaetan@gmail.com>
2025-05-26 22:36:14 +0200

    install vuetify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
+  compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/test-utils']
-})
+  modules: ["@nuxt/eslint", "@nuxt/test-utils"],
+  css: ["vuetify/styles", "@mdi/font/css/materialdesignicons.css"],
+
+  build: {
+    transpile: ["vuetify"],
+  },
+
+  vite: {
+    define: {
+      "process.env.DEBUG": false,
+    },
+  },
+});
